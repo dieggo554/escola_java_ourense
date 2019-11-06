e1760864ba6bbfcc67d2ae99903d3cb0335e4202
Diego <dieggo554@gmail.com>
2019-11-06 15:52:52 +0100

    Hola Mundo en Java.

---
+++ 00_IntroJava/HolaMundo.java
@@ -0,0 +1,10 @@
+class HolaMundo {
+	public static void main(String[] args) {
+		byte unByte = -128;
+		int unInt = 1000;
+		System.out.println("Hola mundo " + "Tipos de datos:");
+		System.out.println("- Byte(-128,0,127 primer bit para signo): " + unByte);
+		System.out.println("- int: " + unInt);
+		//return 0;
+	}
+}
