7a5b0ccf5c5810240afcb042869c70a8ea62c60f
Diego <dieggo554@gmail.com>
2019-11-26 13:41:10 +0100

    -Añadir proyecto Vista que utiliza ModeloUsuarioBBDD.

---
+++ 06_VistaUsuario_JSP/src/java/com/vn/controladores/UsuariosServlet.java
@@ -0,0 +1,97 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.vn.controladores;
+
+import com.vn.POJOs.Usuario;
+import com.vn.servicio.UsuarioServicio;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author pc
+ */
+public class UsuariosServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+        String nombre = request.getParameter("nombre");
+        String edad = request.getParameter("edad");
+        
+        try {
+            if (request.getMethod() == "POST") {
+                Usuario usuario = UsuarioServicio.crear(edad, nombre, email, password);
+                if (usuario != null && usuario.getId() > 0) {
+                    request.getRequestDispatcher("registrado.jsp")
+                            .forward(request, response);
+                } else {
+                    response.getWriter().println("Error al crear");
+                }
+            }
+        } catch (Exception ex) {
+            Logger.getLogger(UsuariosServlet.class.getName()).log(Level.SEVERE, null, ex);
+            response.getWriter().println("ERROR al crear");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
